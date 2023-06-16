dff07eb93de5d389dd53fe72515971daf1bbc628
richi-coder <richicoder@gmail.com>
2023-06-15 23:00:19 -0400

    better stat button

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useRef } from 'react'
 import { useDispatch } from 'react-redux'
 import { initializeCoins, resetDashboard, setGameCondition } from '../reducers/Dashboard';
-import { createPuzzle, nextPuzzle } from '../reducers/Puzzle';
+import { nextPuzzle } from '../reducers/Puzzle';
 import storage from "../services/localStorage"
-import service from "../services/firebase"
 import { resetImageLoad } from '../reducers/ImageLoadReducer';
 import "./styles/startButton.css"
 
@@ -17,7 +16,6 @@ function StartButton() {
         let startAudio = document.querySelector("#start-sound");
         startAudio.play()
         animatedButton.current.classList.add("startButton")
-        //service.uploadChallenges();
         setTimeout(() => {
             dispatch(nextPuzzle(storeChallenge[gameData.level]))
             dispatch(resetDashboard())
@@ -28,7 +26,7 @@ function StartButton() {
     }
 
   return (
-    <button ref={animatedButton} onClick={startGame} className='bg-green-500 py-5 w-1/2 text-3xl border-2 rounded-lg text-white font-bold'>START</button>
+    <button ref={animatedButton} onClick={startGame} className='bg-green-500 py-5 w-1/2 text-3xl border-2 rounded-lg text-white font-bold hover:text-black transition-colors'>START</button>
   )
 }
 
