0697d2bff7879bc475f8faf35b2fa592fbf230ce
richi-coder <richicoder@gmail.com>
2023-06-15 23:10:25 -0400

    not using animation when winning for now

--- src/components/WinnerAnimation.jsx
+++ src/components/WinnerAnimation.jsx
@@ -5,9 +5,9 @@ function WinnerAnimation() {
     const [elements, setElements] = useState(new Array(10).fill(0))
   return (
     <div className='winner-animation'>
-        {
+        {/* {
             elements.map((element,i) => <div key={i} className='animation-element'></div>)
-        }
+        } */}
     </div>
   )
 }
