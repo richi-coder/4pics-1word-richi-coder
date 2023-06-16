b9cf075dd4e31743e141ad10bf4f97daa49f666f
richi-coder <richicoder@gmail.com>
2023-06-15 22:38:34 -0400

    better intro

---
+++ src/components/Music.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useRef, useState } from "react"
+
+function Music() {
+    const musicRef = useRef();
+    let playing = true;
+
+    function playMusic(firstShow) {
+      if (playing) {
+        playing = false; 
+        presentation.classList.add('presentation')
+        firstShow.style.display = 'block';
+        let music = document.querySelector('#music')
+        let music2 = document.querySelector('#music2')
+
+        music2.play()
+        setInterval(() => {
+          music.play()
+        setTimeout(() => { 
+          music2.play()
+        }, 9450 );
+      }, 9450); 
+    }
+    }
+
+    useEffect(() => {
+      let firstShow = document.querySelector('#first-show');
+      let presentation = document.querySelector('#presentation')
+      
+      window.addEventListener('click', () => {
+        playMusic(firstShow)
+        return window.removeEventListener('click', () => playMusic(firstShow) ) 
+        }) 
+      
+      return
+      
+    }, [])
+    
+  return (
+    <>
+    <audio ref={musicRef} id="music" src="music.mp3"></audio>
+    <audio ref={musicRef} id="music2" src="music.mp3"></audio>
+    </>
+  )
+}
+
+export default Music
